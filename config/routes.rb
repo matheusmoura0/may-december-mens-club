81155282faedaeed972218587c59abad95466cd2
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-08-28 13:21:02 -0300

    feat: route registration authentication and member dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
+  resource :registration, only: %i[new create]
+  resource :session, only: %i[new create destroy]
+
+  namespace :members do
+    resource :dashboard, only: :show
+  end
+
   root "home#index"
 end
