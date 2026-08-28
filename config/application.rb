d5900493e80bc02e13738f9c935e21f0d58e6b50
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-08-28 13:08:33 -0300

    feat: add Rails foundation with PostgreSQL

---
+++ config/application.rb
@@ -0,0 +1,14 @@
+require_relative "boot"
+
+require "rails/all"
+
+Bundler.require(*Rails.groups)
+
+module MayDecemberMensClub
+  class Application < Rails::Application
+    config.load_defaults 8.1
+    config.autoload_lib(ignore: %w[assets tasks])
+    config.time_zone = "UTC"
+    config.active_record.default_timezone = :utc
+  end
+end
