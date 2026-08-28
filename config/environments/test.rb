9ab7c5678e331eacb84296bcd48a4c0c4e486909
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-08-28 13:31:24 -0300

    fix: disable forgery protection in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -4,6 +4,7 @@ Rails.application.configure do
   config.public_file_server.enabled = true
   config.consider_all_requests_local = true
   config.action_controller.perform_caching = false
+  config.action_controller.allow_forgery_protection = false
   config.cache_store = :null_store
   config.action_dispatch.show_exceptions = :rescuable
   config.active_support.deprecation = :stderr
