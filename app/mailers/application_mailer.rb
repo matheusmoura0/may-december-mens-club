ce2c48950da46b34fd904c6a721d4238ebcc8072
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-09-02 17:46:00 -0300

    M2 corrections: use controlled sender domain

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch("MAIL_FROM", "no-reply@maydecember.club")
+  default from: ENV.fetch("MAIL_FROM", "no-reply@maydecemberclub.com")
   layout false
 end
