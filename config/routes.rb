bf3ecd3a1f46503a264a80a9810f3a2b0ef88cfd
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-09-02 09:56:30 -0300

    feat: add registration resume and password recovery routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
-  resource :registration, only: %i[new create]
+  resource :registration, only: %i[new create edit update]
   resource :session, only: %i[new create destroy]
+
+  get "/password/reset", to: "password_resets#new", as: :new_password_reset
+  post "/password/reset", to: "password_resets#create", as: :password_reset_requests
+  get "/password/reset/:token", to: "password_resets#edit", as: :edit_password_reset
+  patch "/password/reset/:token", to: "password_resets#update", as: :password_reset
 
   namespace :members do
     resource :dashboard, only: :show
