6b107282b023e6fce0fd3737703479c1d4097a25
Matheus Moura Oliveira <matheusmoura2110@gmail.com>
2026-09-03 19:13:23 -0300

    Use business-controlled SMTP sender configuration

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,8 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: ENV.fetch("MAIL_FROM", "no-reply@maydecemberclub.com")
+  default from: -> {
+    address = ENV.fetch("SMTP_FROM_ADDRESS", ENV.fetch("MAIL_FROM", "no-reply@maydecemberclub.com"))
+    name = ENV["SMTP_FROM_NAME"].presence
+    name ? Mail::Address.new(address).tap { |mail_address| mail_address.display_name = name }.format : address
+  }
   layout false
 end
